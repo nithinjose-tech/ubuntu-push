24a0041f8df20e64add6a80d3dc545c08c042596
nithinjose-tech <ninjos8@gmail.com>
2021-10-11 23:12:17 +0530

    port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get("/:shortUrl", async (req, res) => {
   res.redirect(shortUrl.full);
 });
 
-const PORT = process.env.PORT | 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
